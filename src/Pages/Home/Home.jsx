abae634f12f8e111ba98ae868473fe98f803cb81
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-08-18 23:54:11 -0300

    asking for data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import './home.css'
 const Home = () => {
   return (
     <div className='d-flex flex-column align-items-center'>
-      <h1 className='mt-5'>Bienvenidos a Tu Clima</h1>
-        <h2 className='mt-3'>El lugar donde podras saber si es necesario o no llevar paraguas o abrigo</h2>
+      <h1 className='mt-5'>Bienvenidos a cotizador de cripto</h1>
+        <h2 className='mt-3'>El lugar donde podras saber si es conveniente o no invertir</h2>
         <button className='butt mt-4'>
           <Link to='/mainpage'>Pagina Principal</Link>
         </button>
--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,19 @@
 import './mainPage.css'
+import axios from 'axios'
 
 const MainPage = () => {
+  const Actualizar = async () => {
+    try {
+      const response = await axios.get('http://localhost:8003/')
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error', error)
+    }
+  }
   return (
     <div>
       <h1>Hola</h1>
+      <button onClick={Actualizar}>Actualizar</button>
     </div>
   )
 }
